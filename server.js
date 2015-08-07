75efa090419cd90df1a17a9f9584cfd4d3571609
Ladrillo <gabriel@cabrejas.net>
2015-08-07 16:36:33 -0600

    adds env configuration files

--- server.js
+++ server.js
@@ -1,8 +1,9 @@
 /// <reference path="typings/node/node.d.ts"/>
+// setting the correct environmet
 process.env.NODE_ENV = process.env.NODE_ENV || 'development';
 var port = process.env.PORT || 5000;
 
-
+// bringing the express object from config
 var express = require('./core/server/config/express');
 var app = express();
 
