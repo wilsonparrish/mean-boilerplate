7755b33e46b2c97f0ff034020ad3ef09a5a8f99d
Ladrillo <gabriel@cabrejas.net>
2015-08-07 16:16:41 -0600

    previous broke heroku. Trying to fix...

--- core/server/config/express.js
+++ core/server/config/express.js
@@ -4,18 +4,22 @@ var express = require('express'),
     bodyParser = require('body-parser'),
     methodOverride = require('method-override');
 
+
 module.exports = function () {
     // generates the app object
     var app = express();
 
+
     // block of added environment dependant middleware
+    // not sure if it works with heroku like this
     if (process.env.NODE_ENV === 'development') {
         app.use(morgan('dev'));
     }
     else if (process.env.NODE_ENV === 'production') {
         app.use(compress());
     }
-    
+
+
     // this middleware will work no matter the environment
     app.use(bodyParser.urlencoded(
         {
@@ -23,6 +27,7 @@ module.exports = function () {
         }));
     app.use(bodyParser.json());
     app.use(methodOverride());
+
 
     // we run the router object
     require('../routes/index.server.routes.js')(app);
--- server.js
+++ server.js
@@ -1,22 +1,15 @@
 /// <reference path="typings/node/node.d.ts"/>
 process.env.NODE_ENV = process.env.NODE_ENV || 'development';
+var port = process.env.PORT || 5000;
+
 
 var express = require('./core/server/config/express');
-
 var app = express();
 
-app.listen(5000);
+
+app.listen(port, function () {
+    console.log('listening on ' + port);
+});
+
 
 module.exports = app;
-
-console.log('server running at http://localhost:5000/');
-
-
-
-
-
-// var port = process.env.PORT || 5000;
-//
-// app.listen(port, function () {
-//     console.log('listening on ' + port);
-// });
