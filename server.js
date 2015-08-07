0971aaf4e31901d949b280212dc2838cf0581e43
Ladrillo <gabriel@cabrejas.net>
2015-08-07 15:11:50 -0600

    basic bootstrapping of express app

--- server.js
+++ server.js
@@ -1,15 +1,10 @@
 /// <reference path="typings/node/node.d.ts"/>
-var express = require('express');
-var app = express();
+var express = require('./core/server/config/express');
 
-app.use('/', function (req, res) {
-    res.send('Hello World');
-});
+var app = express();
 
 var port = process.env.PORT || 5000;
 
 app.listen(port, function () {
     console.log('listening on ' + port);
 });
-
-module.exports = app;
