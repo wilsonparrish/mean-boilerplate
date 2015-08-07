0bbf5089cd5054690ba5d182c83fcb281818b9c5
Ladrillo <gabriel@cabrejas.net>
2015-08-07 16:06:29 -0600

    messed with process.env.NODE_ENV. risk!

--- core/server/config/express.js
+++ core/server/config/express.js
@@ -1,7 +1,30 @@
-var express = require('express');
+var express = require('express'),
+    morgan = require('morgan'),
+    compress = require('compression'),
+    bodyParser = require('body-parser'),
+    methodOverride = require('method-override');
 
 module.exports = function () {
+    // generates the app object
     var app = express();
+
+    // block of added environment dependant middleware
+    if (process.env.NODE_ENV === 'development') {
+        app.use(morgan('dev'));
+    }
+    else if (process.env.NODE_ENV === 'production') {
+        app.use(compress());
+    }
+    
+    // this middleware will work no matter the environment
+    app.use(bodyParser.urlencoded(
+        {
+            extended: true
+        }));
+    app.use(bodyParser.json());
+    app.use(methodOverride());
+
+    // we run the router object
     require('../routes/index.server.routes.js')(app);
     return app;
 };
--- server.js
+++ server.js
@@ -1,10 +1,22 @@
 /// <reference path="typings/node/node.d.ts"/>
+process.env.NODE_ENV = process.env.NODE_ENV || 'development';
+
 var express = require('./core/server/config/express');
 
 var app = express();
 
-var port = process.env.PORT || 5000;
+app.listen(5000);
 
-app.listen(port, function () {
-    console.log('listening on ' + port);
-});
+module.exports = app;
+
+console.log('server running at http://localhost:5000/');
+
+
+
+
+
+// var port = process.env.PORT || 5000;
+//
+// app.listen(port, function () {
+//     console.log('listening on ' + port);
+// });
