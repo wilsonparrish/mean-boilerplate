c293e2a63cf47746aa0620e79eb195d16d4aa4b2
Ladrillo <gabriel@cabrejas.net>
2015-08-09 17:39:36 -0600

    cloud mongoLab works great with localhost, heroku broken?

--- core/server/config/env/development.js
+++ core/server/config/env/development.js
@@ -1,4 +1,5 @@
 module.exports = {
-    db: 'mongodb://localhost/mean-boilerplate',
+    // db: 'mongodb://localhost/mean-boilerplate',
+    db: 'mongodb://Ladrillo:Mongogabro01@ds031223.mongolab.com:31223/heroku_q398n5nn',
     sessionSecret: 'developmentSessionSecret'
 };
--- core/server/config/env/production.js
+++ core/server/config/env/production.js
@@ -1,4 +1,4 @@
 module.exports = {
-    db: 'mongodb://Ladrillo:Mongogabro01@ds031223.mongolab.com:31223/heroku_q398n5nn',
+    db: 'mongodb://heroku_q398n5nn:Mongogabro01@ds031223.mongolab.com:31223/heroku_q398n5nn',
     sessionSecret: 'productionSessionSecret'
 };
